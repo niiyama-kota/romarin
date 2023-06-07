51a3931a09a5b70b47a2e0b1e880a487883d2587
niiyama-kota <k0o4t2a5@gmail.com>
2023-06-07 10:08:02 +0900

    run simple net

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use tch::Tensor;
+use romarin::transistor_model;
 
 fn main() {
-    let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
-    let t = t * 2;
-    t.print();
+    // let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
+    // let t = t * 2;
+    // t.print();
+
+    let err = transistor_model::simple_net::run();
+    println!("{:?}", err);
 }
