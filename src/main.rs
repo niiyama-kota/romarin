796e5623d3fbf339e18e1c997a03e6c11ade4f48
niiyama-kota <k0o4t2a5@gmail.com>
2023-06-08 15:35:06 +0900

    implement PiNN

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use tch::Tensor;
+use romarin::transistor_model::PiNN;
 
 fn main() {
-    let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
-    let t = t * 2;
-    t.print();
+    // let t = Tensor::from_slice(&[3, 1, 4, 1, 5, 6]);
+    // let t = t.view([2, 3, 1]);
+    // t.tanh().print();
+
+    let _ = PiNN::run();
 }
--- src/transistor_model/PiNN.rs
+++ src/transistor_model/PiNN.rs
@@ -1,75 +1,154 @@
+use crate::loader;
 use anyhow::Result;
 use tch::{
     nn,
     nn::OptimizerConfig,
     nn::{Func, Module},
-    Device, Tensor,
+    Device, Kind, Tensor,
 };
 
-const VD_NEURON_NUM: i64 = 3;
-const VG_NEURON_NUM: i64 = 2;
+const VD_NEURON_NUM: i64 = 2;
+const VG_NEURON_NUM: i64 = 3;
 
 #[derive(Debug)]
-pub struct PiNN_net<'a> {
-    // vd_sub_layer0: nn::Linear,
-    // vd_sub_layer1: nn::Linear,
-    // vg_sub_layer0: nn::Linear,
-    // vg_sub_layer1: nn::Linear,
-    layer0: Func<'a>,
-    layer1: Func<'a>,
+struct Vd_subnet {
+    vd_sub_layer0: nn::Linear,
+    vd_sub_layer1: nn::Linear,
 }
 
-impl<'a> PiNN_net<'a> {
-    fn new(vs: &nn::Path) -> PiNN_net<'a> {
-        // stride -- padding -- dilation
-        let vd_sub_layer0 = nn::linear(vs / "vd_subnet_layer0", 1, VD_NEURON_NUM, Default::default());
-        let vd_sub_layer1 = nn::linear(vs / "vd_subnet_layer1", 1, VD_NEURON_NUM, Default::default());
-        let vg_sub_layer0 = nn::linear(vs / "vg_subnet_layer0", 1, VG_NEURON_NUM, Default::default());
-        let vg_sub_layer1 = nn::linear(vs / "vg_subnet_layer1", 1, VG_NEURON_NUM, Default::default());
-        let layer0 = nn::func::<'a>(|tensor| {
-            let v = tensor.view([2]);
-            let vd_out = v.get(0).apply(&vd_sub_layer0).tanh();
-            let vg_out = v.get(1).apply(&vg_sub_layer0).sigmoid();
-            let v = Tensor::cat(&[vd_out, vd_out + vg_out], 0);
-            v
-        });
-        let layer1 = nn::func(|tensor| {
-            let v = tensor.view([2]);
-            let vd_out = v.get(0).apply(&vd_sub_layer1).tanh();
-            let vg_out = v.get(1).apply(&vg_sub_layer1).sigmoid();
-            let v = Tensor::cat(&[vd_out, vd_out + vg_out], 0);
-            v
-        });
-        PiNN_net {
-            layer0,
-            layer1,
+impl Vd_subnet {
+    fn new(vs: &nn::Path) -> Vd_subnet {
+        let vd_sub_layer0 = nn::linear(
+            vs / "vd_subnet_layer0",
+            1,
+            VD_NEURON_NUM,
+            Default::default(),
+        );
+        let vd_sub_layer1 = nn::linear(
+            vs / "vd_subnet_layer1",
+            VD_NEURON_NUM,
+            1,
+            Default::default(),
+        );
+
+        Vd_subnet {
+            vd_sub_layer0,
+            vd_sub_layer1,
         }
     }
 }
 
-impl<'a> Module for PiNN_net<'a> {
-    fn forward(&self, xs: &tch::Tensor) -> tch::Tensor {}
+#[derive(Debug)]
+struct Vg_subnet {
+    vg_sub_layer0: nn::Linear,
+    vg_sub_layer1: nn::Linear,
+}
+
+impl Vg_subnet {
+    fn new(vs: &nn::Path) -> Vg_subnet {
+        let vg_sub_layer0 = nn::linear(
+            vs / "vg_subnet_layer0",
+            1,
+            VG_NEURON_NUM,
+            Default::default(),
+        );
+        let vg_sub_layer1 = nn::linear(
+            vs / "vg_subnet_layer1",
+            VG_NEURON_NUM,
+            1,
+            Default::default(),
+        );
+
+        Vg_subnet {
+            vg_sub_layer0,
+            vg_sub_layer1,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct PiNN_net {
+    vd_subnet: Vd_subnet,
+    vg_subnet: Vg_subnet,
+    vd2vg_layer0: nn::Linear,
+    vd2vg_layer1: nn::Linear,
+}
+
+impl PiNN_net {
+    fn new(vs: &nn::Path) -> PiNN_net {
+        let vd_subnet = Vd_subnet::new(vs);
+        let vg_subnet = Vg_subnet::new(vs);
+        let vd2vg_layer0 = nn::linear(
+            vs / "vd2vg_layer0",
+            VD_NEURON_NUM,
+            VG_NEURON_NUM,
+            Default::default(),
+        );
+        let vd2vg_layer1 = nn::linear(
+            vs / "vd2vg_layer1",
+            1,
+            1,
+            Default::default(),
+        );
+
+        PiNN_net {
+            vd_subnet,
+            vg_subnet,
+            vd2vg_layer0,
+            vd2vg_layer1,
+        }
+    }
+}
+
+impl Module for PiNN_net {
+    fn forward(&self, xs: &tch::Tensor) -> tch::Tensor {
+        let vd = xs.select(1, 0).view([-1, 1]);
+        let vg = xs.select(1, 1).view([-1, 1]);
+        let t = vd.apply(&self.vd_subnet.vd_sub_layer0).tanh();
+        let s = (t.apply(&self.vd2vg_layer0) + vg.apply(&self.vg_subnet.vg_sub_layer0)).sigmoid();
+        let t = t.apply(&self.vd_subnet.vd_sub_layer1);
+        let s = (t.apply(&self.vd2vg_layer1) + s.apply(&self.vg_subnet.vg_sub_layer1)).sigmoid();
+
+        &t * &s
+    }
 }
 
 pub fn run() -> Result<()> {
-    let m = tch::vision::mnist::load_dir("data")?;
+    let dataset = loader::read_csv("data/integral_train.csv".to_string()).unwrap();
+    let x = Tensor::stack(
+        &[
+            Tensor::from_slice(dataset.VDS.as_slice()),
+            Tensor::from_slice(dataset.VGS.as_slice()),
+        ],
+        1,
+    ).to_kind(Kind::Float);
+    let y = Tensor::from_slice(dataset.IDS.as_slice()).to_kind(Kind::Float);
     let vs = nn::VarStore::new(Device::Cpu);
-    let net = net(&vs.root());
-    let mut opt = nn::AdamW::default().build(&vs, 1e-3)?;
-    for epoch in 1..200 {
-        let loss = net
-            .forward(&m.train_images)
-            .cross_entropy_for_logits(&m.train_labels);
+    let net = PiNN_net::new(&vs.root());
+    let mut opt = nn::Adam::default().build(&vs, 1e-3)?;
+    let mut losses = Vec::<Tensor>::new();
+    for epoch in 1..=10000 {
+        let loss = net.forward(&x).mse_loss(&y, tch::Reduction::Mean);
         opt.backward_step(&loss);
-        let test_accuracy = net
-            .forward(&m.test_images)
-            .accuracy_for_logits(&m.test_labels);
-        println!(
-            "epoch: {:4} train loss: {:8.5} test acc: {:5.2}%",
-            epoch,
-            f64::from(&loss),
-            100. * f64::from(&test_accuracy),
-        );
+        losses.push(loss);
+        println!("epoch {}", epoch);
     }
+
+    println!("initial loss: {:?} \n last loss: {:?}", losses.first(), losses.last());
     Ok(())
+}
+
+#[test]
+fn tensor_concat_test() {
+    let dataset = loader::read_csv("data/integral_train.csv".to_string()).unwrap();
+    let vs = nn::VarStore::new(Device::Cpu);
+    let t = &Tensor::stack(
+        &[
+            Tensor::from_slice(dataset.VDS.as_slice()),
+            Tensor::from_slice(dataset.VGS.as_slice()),
+        ],
+        1,
+    );
+
+    println!("{:?}", t.view([2, 292]));
 }
