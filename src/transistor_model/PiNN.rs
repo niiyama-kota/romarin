76feda3372fbc08636a2fff8bdd2a994d93e2ed5
niiyama-kota <k0o4t2a5@gmail.com>
2023-06-19 16:00:15 +0900

    implement rmse calculation

--- src/transistor_model/PiNN.rs
+++ src/transistor_model/PiNN.rs
@@ -7,8 +7,8 @@ use tch::{
     Device, Kind, Tensor,
 };
 
-const VD_NEURON_NUM: i64 = 200;
-const VG_NEURON_NUM: i64 = 300;
+const VD_NEURON_NUM: i64 = 2;
+const VG_NEURON_NUM: i64 = 3;
 
 #[derive(Debug)]
 struct Vd_subnet {
@@ -93,6 +93,10 @@ impl PiNN {
             vd2vg_layer1,
         }
     }
+
+    fn rmse(&self, xs: &tch::Tensor, ys: &tch::Tensor) -> tch::Tensor {
+        (ys - self.forward(xs)).square().mean(Kind::Float).sqrt()
+    }
 }
 
 impl Module for PiNN {
@@ -118,18 +122,29 @@ pub fn run() -> Result<()> {
         ],
         1,
     )
-    // .set_requires_grad(true)
     .to_kind(Kind::Float);
     let y = Tensor::from_slice(dataset.IDS.as_slice())
-    // .set_requires_grad(true)
     .to_kind(Kind::Float);
+
+    let mut test_dataset = loader::read_csv("data/integral.csv".to_string()).unwrap();
+    test_dataset.min_max_scaling();
+    let x_test = Tensor::stack(
+        &[
+            Tensor::from_slice(test_dataset.VDS.as_slice()),
+            Tensor::from_slice(test_dataset.VGS.as_slice()),
+        ],
+        1,
+    )
+    .to_kind(Kind::Float);
+    let y_test = Tensor::from_slice(test_dataset.IDS.as_slice()).to_kind(Kind::Float);
+
     let vs = nn::VarStore::new(Device::Cpu);
     let net = PiNN::new(&vs.root());
     let mut opt = nn::RmsProp::default().build(&vs, 1e-3)?;
     let mut losses = Vec::<Tensor>::new();
     for epoch in 1..=10000 {
         let loss = (net.forward(&x) - &y)
-            .pow_tensor_scalar(2)
+            .square()
             .mean(Kind::Float);
         loss.print();
         opt.backward_step(&loss);
@@ -142,7 +157,10 @@ pub fn run() -> Result<()> {
         losses.first(),
         losses.last()
     );
-    println!("{:?}", y.grad());
+
+    let rmse = net.rmse(&x_test, &y_test);
+    println!("test rmse: {:?}", rmse);
+    
     Ok(())
 }
 
--- src/transistor_model/simple_net.rs
+++ src/transistor_model/simple_net.rs
@@ -1,32 +1,50 @@
 use anyhow::Result;
 use tch::{nn, nn::Module, nn::OptimizerConfig, Device, Kind, Tensor};
 
-use crate::{loader, transistor_model::simple_net};
+use crate::loader;
 
 const NEURON_NUM: i64 = 500;
 
-fn new(vs: &nn::Path) -> impl Module {
-    nn::seq()
-        .add(nn::linear(
-            vs / "input layer",
-            2,
-            NEURON_NUM,
-            Default::default(),
-        ))
-        .add_fn(|xs| xs.relu())
-        .add(nn::linear(
-            vs / "layer1",
-            NEURON_NUM,
-            NEURON_NUM,
-            Default::default(),
-        ))
-        .add_fn(|xs| xs.relu())
-        .add(nn::linear(
-            vs / "output layer",
-            NEURON_NUM,
-            1,
-            Default::default(),
-        ))
+#[derive(Debug)]
+struct simple_net {
+    layers: nn::Sequential,
+}
+impl simple_net {
+    fn new(vs: &nn::Path) -> Self {
+        Self {
+            layers: nn::seq()
+                .add(nn::linear(
+                    vs / "input layer",
+                    2,
+                    NEURON_NUM,
+                    Default::default(),
+                ))
+                .add_fn(|xs| xs.relu())
+                .add(nn::linear(
+                    vs / "layer1",
+                    NEURON_NUM,
+                    NEURON_NUM,
+                    Default::default(),
+                ))
+                .add_fn(|xs| xs.relu())
+                .add(nn::linear(
+                    vs / "output layer",
+                    NEURON_NUM,
+                    1,
+                    Default::default(),
+                )),
+        }
+    }
+
+    fn rmse(&self, xs: &tch::Tensor, ys: &tch::Tensor) -> tch::Tensor {
+        (ys - self.forward(xs)).square().mean(Kind::Float).sqrt()
+    }
+}
+
+impl Module for simple_net {
+    fn forward(&self, xs: &Tensor) -> Tensor {
+        self.layers.forward(xs)
+    }
 }
 
 pub fn run() -> Result<()> {
@@ -40,8 +58,20 @@ pub fn run() -> Result<()> {
         1,
     )
     .to_kind(Kind::Float);
-    let y = Tensor::from_slice(dataset.IDS.as_slice())
-        .to_kind(Kind::Float);
+    let y = Tensor::from_slice(dataset.IDS.as_slice()).to_kind(Kind::Float);
+
+    let mut test_dataset = loader::read_csv("data/integral.csv".to_string()).unwrap();
+    test_dataset.min_max_scaling();
+    let x_test = Tensor::stack(
+        &[
+            Tensor::from_slice(test_dataset.VDS.as_slice()),
+            Tensor::from_slice(test_dataset.VGS.as_slice()),
+        ],
+        1,
+    )
+    .to_kind(Kind::Float);
+    let y_test = Tensor::from_slice(test_dataset.IDS.as_slice()).to_kind(Kind::Float);
+
     let vs = nn::VarStore::new(Device::Cpu);
     let net = simple_net::new(&vs.root());
     let mut opt = nn::RmsProp::default().build(&vs, 1e-3)?;
@@ -63,6 +93,9 @@ pub fn run() -> Result<()> {
         losses.first(),
         losses.last()
     );
+
+    let rmse = net.rmse(&x_test, &y_test);
+    println!("test rmse: {:?}", rmse);
 
     Ok(())
 }
