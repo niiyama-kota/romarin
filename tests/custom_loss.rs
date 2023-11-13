81082fbd14420ff39608a035314d1381717afd2d
niiyama-kota <k0o4t2a5@gmail.com>
2023-11-14 02:22:32 +0900

    WIP: check backward and grad working

--- tests/custom_loss.rs
+++ tests/custom_loss.rs
@@ -1,41 +1,51 @@
 use tch::{
-    nn::{self, LinearConfig, Module, OptimizerConfig},
+    nn::{self, OptimizerConfig},
     Device, Kind, Tensor,
 };
 
 #[test]
 fn test_custom_loss() {
-    let vs = nn::VarStore::new(Device::Cpu);
     let x = Tensor::from_slice2(&[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
         .reshape([-1, 5])
         .to_kind(Kind::Float)
         .set_requires_grad(true);
     // y = sum(xi^2)
-    let y = Tensor::from_slice2(&[[1 + 4 + 9 + 16 + 25], [36 + 49 + 64 + 81 + 100]])
-        .reshape([-1, 1])
+    let y = Tensor::from_slice2(&[[2, 4, 6, 8, 10], [12, 14, 16, 18, 20]])
+        .reshape([-1, 5])
         .to_kind(Kind::Float)
         .set_requires_grad(true);
-    let lin1 = nn::linear(&vs.root(), 5, 100, LinearConfig::default());
-    let lin2 = nn::linear(&vs.root(), 100, 1, LinearConfig::default());
+    let vs = nn::VarStore::new(Device::Cpu);
+    let k = vs
+        .root()
+        .f_var("coef", &[], nn::Init::Uniform { lo: 0f64, up: 1f64 })
+        .unwrap();
+    println!("{:?}", vs.variables());
 
-    let epoch = 10000;
-    let lr = 1e-2;
-    let mut opt = nn::AdamW::default().build(&vs, lr).unwrap();
-
-    for _epoch in 1..=epoch {
-        println!("epoch {}", _epoch);
+    let mut opt = nn::AdamW::default().build(&vs, 5e-4).unwrap();
+    for _ in 0..=10000 {
         opt.zero_grad();
-        let t = lin1.forward(&x).sigmoid();
-        let t = lin2.forward(&t).relu();
-        let loss = t.mse_loss(&y, tch::Reduction::Mean);
-        println!("loss: {}", loss.double_value(&[]));
+        let out = &k * &x;
+        let loss = ((&out - &y) * (&out - &y)).mean(Some(Kind::Float));
         loss.backward();
-        {
-            println!("x grad: ");
-            x.grad().print();
-            println!("");
-        }
         opt.step();
-        // opt.backward_step(&loss);
+        loss.print();
+        k.grad().print();
     }
+    (&k * &x).print();
+    x.print();
+    y.print();
+    println!("");
+}
+
+#[test]
+fn test_simple_grad() {
+    let x = Tensor::ones(&[2, 2], (Kind::Float, Device::Cpu)).set_requires_grad(true);
+    let y = &x + 2;
+    y.print();
+    let z = &y * &y * 3;
+    let out = z.mean(Some(Kind::Float));
+    z.print();
+    out.print();
+    out.backward();
+    x.grad().print();
 }
